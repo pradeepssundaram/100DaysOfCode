f45231571454299b23474f59e98d830c7e436c2c
Pradeep Sundaram <pradeepssundaram@gmail.com>
2020-09-16 16:14:34 +0530

    Added all EDAs for bike sharing and started of with t-test

--- RCode/BikeSharing/Exercise1.01.R
+++ RCode/BikeSharing/Exercise1.01.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(magrittr)
 
-bikesharing <- read_csv("./Data/day.csv")
+bikesharing <- read_csv("./Data/hour.csv")
 # yr_mapping = {0: "2011", 1:"2012"}
 # mnth_mapping = {1:"Jan",2:"Feb",3:"Mar",4:"Apr",5:"May",6:"Jun",7:"Jul",8:"Aug",9:"Sep",10:"Oct",11:"Nov",12:"Dec"}
 # weekday_mapping={0: 'Sunday', 1: 'Monday', 2: 'Tuesday',3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday'}
@@ -50,91 +50,147 @@ bikesharing %<>% mutate(
                         
                           )
 
-bikeshare_plot<-bikesharing %>% pivot_longer(cols = c(registered,casual),names_to="RideType",values_to="NumRides") %>% select(RideType,NumRides)
+# plot_data<-bikesharing %>% pivot_longer(cols=c(registered,casual),names_to="RideType",values_to="NumberOfRides") %>% 
+#   select(weekday,RideType,NumberOfRides)
+#by season by hour
 
-fig1<-ggplot(data = bikesharing) +
-    geom_density(mapping = aes(x=registered))+
-  geom_density(mapping = aes(x=casual))
-fig1 +   scale_y_continuous(labels=function(n){format(n, scientific = FALSE)})
+plot_data<-bikesharing %>% pivot_longer(cols =c(registered,casual),names_to="RideType",values_to="NumberOfRides") %>% 
+  select(hr,season,RideType,NumberOfRides) %>% mutate(season=factor(season,levels=c("winter","spring","summer","fall")),RideType=factor(RideType,levels=c("registered","casual")), hr=factor(hr))
 
-fig <- ggplot(data = bikeshare_plot) +
-  geom_density(mapping = aes(x=NumRides,fill=RideType),alpha=0.25) +
-  scale_y_continuous(labels=function(n){format(n, scientific = FALSE)})+
-  theme_classic() +
-  theme(
-    legend.position = c(.95, .95),
-    legend.justification = c("right", "top"),
-    legend.box.just = "right",
-    legend.margin = margin(6, 6, 6, 6)
-  )
+plot_data_average <- 
+  bikesharing %>% group_by(hr,season) %>% summarise(registered=mean(registered),casual=mean(casual)) %>% 
+  pivot_longer(cols =c(registered,casual),names_to="RideType",values_to="AverageRides") %>% 
+  select(hr,season,RideType,AverageRides) %>% 
+  mutate(season=factor(season,levels=c("winter","spring","summer","fall"))
+         ,RideType=factor(RideType,levels=c("registered","casual"))
+         , hr=factor(hr))
 
-fig
+ggplot(data = plot_data_average) + 
+  geom_col(mapping = aes(x=hr,y=AverageRides),fill="lightblue")+ 
+  scale_x_discrete(name="hour", breaks=seq(0,23))+
+  facet_grid(rows = vars(season),cols = vars(RideType),switch = "y") +
+  theme_classic()
 
 
-fig <- ggplot(data = bikeshare_plot) +
-  geom_density(mapping = aes(x=NumRides,fill=RideType),alpha=0.25) +
-  scale_fill_manual( values = c("red","orange"))+
+
+
+
+ggplot(data = plot_data) + 
+  geom_col(mapping = aes(x=hr,y=NumberOfRides),fill="orange")+ 
+  scale_x_discrete(name="hour", breaks=seq(0,23))
+
+
+ggplot(data = plot_data) + 
+  geom_col(mapping = aes(x=hr,y=NumberOfRides),fill="lightblue")+ 
+  scale_x_discrete(name="hour", breaks=seq(0,23))+
+  facet_grid(rows = vars(season),cols = vars(RideType),switch = "y") +
+  theme_classic()
+
+ggplot(data = plot_data) + 
+  geom_bar(mapping = aes(x=hr,y=NumberOfRides),fill="lightblue",stat="identity")+ 
+  facet_grid(rows = vars(season),cols = vars(RideType)) +
+  theme_classic()
+
+
+
+
+plot_data<-bikesharing %>% pivot_longer(cols =c(registered,casual),names_to="RideType",values_to="NumberOfRides") %>% 
+  select(weekday,season,RideType,NumberOfRides) %>% mutate(season=factor(season,levels=c("winter","spring","summer","fall"))
+                                                   ,RideType=factor(RideType,levels=c("registered","casual"))
+                                                  ,weekday=factor(weekday,levels = c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))
+                                                )
+  
+ggplot(data = plot_data) +
+  geom_col(mapping = aes(x=weekday,y=NumberOfRides),fill="lightblue") +
+  facet_grid(rows = vars(season),cols = vars(RideType)) +
   scale_y_continuous(labels=function(n){format(n, scientific = FALSE)})+
   theme_classic()
-fig
 
 
-# 
-# 
-# fig <- ggplot(data = bikesharing) + 
-#   geom_density(mapping = aes(x=registered,colour="red")) +
-#   geom_density(mapping = aes(x=casual,colour="blue")) +
-#   theme_classic() +
-#   theme(panel.grid.major.x = element_blank()) +
-#   theme(panel.grid.minor.x = element_blank()) +
-#   theme(panel.grid.major.y = element_blank()) +
-#   theme(panel.grid.minor.y = element_blank()) +
-#   # scale_x_discrete(limit=c(1960,1970,1980,1990,2000,2010),position = "bottom") +
-#   # scale_y_discrete(limit=c(2,4,6,8,10,12)) +
-#   #theme(panel.background =   element_rect(fill = "#B3000C"))+
-#   #theme(panel.background =   element_rect(fill = "#00B32C"))+
-#   #theme(panel.background =   element_rect(fill = "#EF0B3D"))+
-#   #theme(plot.background = element_rect(fill = "#D8D8D8"))+
-#   labs(title = "Distribution of Registered and Casual Riders ",
-#        x="Rides",y="")+
-#   theme(plot.title = element_text(size=10, face="bold", vjust=1, lineheight=0.6))
-#   #theme(plot.title = element_text(size=10, face="bold", vjust=1, lineheight=0.6,colour = "#00B32C"))
-# # +
-# #   theme(
-# #     axis.title.x = element_text(color="#00B32C", vjust=-0.35),
-# #     axis.title.y = element_text(color="#00B32C" , vjust=0.35) ,
-# #     axis.text = element_text(color="#00B32C")
-# #   )
-# fig
-# 
-# #ggsave("./2019-12-24/Figures/ChristmasTrend.png",plot = myplot,dpi = "retina")
-
-
-
-# 
-# import matplotlib.pyplot as plt
-# import seaborn as sns, numpy as np
-# 
-# sns.set(rc={"figure.figsize": (8, 4)}); np.random.seed(0)
-# x = np.random.randn(100)
-# ax = sns.distplot(x)
-# plt.show()
-
-
-
-plot_data<- bikesharing %>% select(dteday,registered,casual) %>% 
-  group_by(dteday) %>% summarise(registered=sum(registered),casual=sum(casual)) %>% 
-  pivot_longer(cols=c(registered,casual),names_to="RideType",values_to="NumberOfRides")
-
-#plot_data %>% slice_sample(n = 100)
-
-fig<-ggplot(data = plot_data) + 
-  geom_line(mapping = aes(x=dteday,y=NumberOfRides,color=RideType))+
-  theme_classic()+
-  theme(
-    legend.position = c(1, 1),
-    legend.justification = c("right", "top"),
-    legend.box.just = "right",
-    legend.margin = margin(0, 0, 0, 0)
+plot_data <- bikesharing %>% group_by(weekday,season) %>% 
+             summarise(registered=mean(registered),casual=mean(casual)) %>% 
+            pivot_longer(cols = c(registered,casual),names_to="RideType",values_to="AverageRides") %>% 
+  mutate(season=factor(season,levels=c("winter","spring","summer","fall"))
+         ,RideType=factor(RideType,levels=c("registered","casual"))
+         ,weekday=factor(weekday,levels = c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))
   )
-fig
+            
+            
+ggplot(data = plot_data) +
+  geom_col(mapping = aes(x=weekday,y=AverageRides),fill="lightblue") +
+  facet_grid(rows = vars(season),cols = vars(RideType)) +
+  scale_y_continuous(labels=function(n){format(n, scientific = FALSE)})+
+  theme_classic()
+
+
+
+ttestData<-bikesharing %>% 
+  select(weekday,registered,casual) %>% 
+  mutate(IsWeekDay=case_when(
+    weekday %in% c("Saturday","Sunday") ~ "No",
+    TRUE~"Yes"
+  )) %>% 
+  select(IsWeekDay,casual,registered) %>% 
+  pivot_longer(cols=c(registered,casual),names_to="RideType",values_to="NumRides")
+
+testResults<-ttestData %>% 
+  group_by(RideType,IsWeekDay) %>% 
+  nest() %>% 
+  spread(key =IsWeekDay,value =data  ) %>% 
+  mutate(
+    t_test = map2(No, Yes, ~{t.test(.x$NumRides, .y$NumRides) %>% tidy()})
+    
+  ) %>%
+  unnest(c(t_test)) %>% 
+  select(RideType,statistic,p.value)
+
+testResults
+
+# df %>% spread(key, value) is equivalent to df %>% pivot_wider(names_from = key, values_from = value)
+
+testResults<-ttestData %>% 
+  group_by(RideType,IsWeekDay) %>% 
+  nest() %>% 
+  pivot_wider(names_from = IsWeekDay,values_from=data) %>% 
+  mutate(
+    t_test = map2(No, Yes, ~{t.test(.x$NumRides, .y$NumRides) %>% tidy()})
+    
+  ) %>%
+  unnest(c(t_test)) %>% 
+  select(RideType,statistic,p.value)
+
+testResults
+
+
+plot_data<- bikesharing %>% select(weekday,registered)
+
+
+# %>% filter(!weekday %in%c("Saturday","Sunday") )
+# tesst1<- t.test(ttestData %>%  filter(IsWeekDay==TRUE ) %>% select(registered),
+#                 ttestData %>%  filter(IsWeekDay==FALSE) %>% select(registered),var.equal = FALSE
+#               ) %>% tidy()
+# View(tesst1)
+
+# category= registered or casual (2)
+# group = IsWeekDay
+# value = numrides
+
+ttestData %>% 
+  group_by(RideType,IsWeekDay) %>% 
+  nest() %>% 
+  spread(key =IsWeekDay,value =data  ) %>% 
+  mutate(
+    t_test = map2(No, Yes, ~{t.test(.x$NumRides, .y$NumRides) %>% tidy()})
+   
+  ) %>% 
+  unnest() 
+
+         
+  
+  ttestData %>%  filter(is(IsWeekDay) %>% select(registered) ,ttestData %>% filter(!is(IsWeekDay) %>% select(registered) ))
+
+  
+
+
+
+
